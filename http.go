fea0517ef4b0ff36d4b20814c9d32030931dcf82
liut <liutao@liut.cc>
2019-12-10 18:39:59 +0800

    Add Middleware(opts ...OptFunc) instead of WithRedirect() and WithUnauthorized()

--- http.go
+++ http.go
@@ -21,47 +21,76 @@ const (
 	UserKey ctxKey = iota
 )
 
-// WithRedirect ...
-func WithRedirect(uri string) func(next http.Handler) http.Handler {
-	return func(next http.Handler) http.Handler {
-		hf := func(w http.ResponseWriter, r *http.Request) {
-			user, err := UserFromRequest(r)
-			if err != nil {
-				http.Redirect(w, r, uri, http.StatusFound)
-				return
-			}
+// Option ...
+type Option struct {
+	uri     string
+	refresh bool
+}
 
-			next.ServeHTTP(w, r.WithContext(context.WithValue(r.Context(), UserKey, user)))
-		}
-		return http.HandlerFunc(hf)
+// OptFunc ...
+type OptFunc func(opt *Option)
+
+// WithURI The option with redirect uri
+func WithURI(uri string) OptFunc {
+	return func(opt *Option) {
+		opt.uri = uri
 	}
 }
 
-// WithUnauthorized ...
-func WithUnauthorized() func(next http.Handler) http.Handler {
+// WithRefresh The option with auto refresh
+func WithRefresh() OptFunc {
+	return func(opt *Option) {
+		opt.refresh = true
+	}
+}
+
+// Middleware ...
+func Middleware(opts ...OptFunc) func(next http.Handler) http.Handler {
+	var option Option
+	for _, fn := range opts {
+		fn(&option)
+	}
 	return func(next http.Handler) http.Handler {
-		hf := func(w http.ResponseWriter, r *http.Request) {
-			user, err := UserFromRequest(r)
+		return http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+			user, err := UserFromRequest(req)
 			if err != nil {
-				http.Error(w, http.StatusText(http.StatusUnauthorized), http.StatusUnauthorized)
+				if option.uri != "" {
+					http.Redirect(rw, req, option.uri, http.StatusFound)
+				} else {
+					http.Error(rw, http.StatusText(http.StatusUnauthorized), http.StatusUnauthorized)
+				}
 				return
 			}
+			if option.refresh && user.NeedRefresh() {
+				user.Refresh()
+				user.Signin(rw) // TODO: custom cookieName somethings
+			}
 
-			next.ServeHTTP(w, r.WithContext(context.WithValue(r.Context(), UserKey, user)))
-		}
-		return http.HandlerFunc(hf)
+			req = req.WithContext(context.WithValue(req.Context(), UserKey, user))
+			next.ServeHTTP(rw, req)
+		})
 	}
+}
+
+// WithRedirect ... Deprecated by Middleware(WithURI(uri))
+func WithRedirect(uri string) func(next http.Handler) http.Handler {
+	return Middleware(WithURI(uri))
+}
+
+// WithUnauthorized ... Deprecated by Middleware()
+func WithUnauthorized() func(next http.Handler) http.Handler {
+	return Middleware()
 }
 
 // UserFromContext ...
-func UserFromContext(ctx context.Context) *User {
+func UserFromContext(ctx context.Context) (*User, bool) {
 	if ctx == nil {
-		return nil
+		return nil, false
 	}
 	if obj, ok := ctx.Value(UserKey).(*User); ok {
-		return obj
+		return obj, true
 	}
-	return nil
+	return nil, false
 }
 
 // UserFromRequest get user from cookie
@@ -79,6 +108,7 @@ func UserFromRequest(r *http.Request) (user *User, err error) {
 		return
 	}
 	if user.IsExpired() {
+		log.Printf("user %s is expired", user.UID)
 		err = fmt.Errorf("user %s is expired", user.UID)
 	}
 	// log.Printf("got user %v", user)
---
+++ http_test.go
@@ -0,0 +1,71 @@
+package auth
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
+
+func TestHTTPSignin(t *testing.T) {
+	var user = &User{
+		UID:  "testUID",
+		Name: "testName",
+	}
+
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		user.Refresh()
+		user.Signin(w)
+		Signin(user, w)
+		w.WriteHeader(http.StatusAccepted)
+	}))
+	defer ts.Close()
+
+	res, err := http.Get(ts.URL)
+	if err != nil {
+		t.Fatalf("get fail %s", err)
+	}
+	if res.StatusCode != http.StatusAccepted {
+		t.Fatalf("invalid status %d", res.StatusCode)
+	}
+	t.Logf("res status %d", res.StatusCode)
+	// t.Logf("res header %v", res.Header)
+	// t.Logf("res cookies %v", res.Cookies())
+
+}
+
+func TestHTTPBack(t *testing.T) {
+	var user = &User{
+		UID:  "testUID",
+		Name: "testName",
+	}
+	// user.Refresh()
+	user.LastHit = time.Now().Unix() - UserLifetime + 10
+	token, err := user.Encode()
+	if err != nil {
+		t.Fatalf("encode fail %s", err)
+	}
+	t.Logf("refresh: %d, token: %s", user.LastHit, token)
+
+	mw := Middleware(WithRefresh())
+	ts := httptest.NewServer(mw(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		user, ok := UserFromContext(r.Context())
+		if !ok {
+			t.Fatal("userFromContext fail")
+		}
+		t.Logf("user %v", user)
+	})))
+	defer ts.Close()
+
+	req, _ := http.NewRequest("GET", ts.URL, nil)
+	// req.Header.Add("Authorization", "Bearer "+token)
+	req.AddCookie(&http.Cookie{Name: CookieName, Value: token})
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		t.Fatalf("get fail %s", err)
+	}
+	if res.StatusCode != http.StatusOK {
+		t.Fatalf("invalid status %d", res.StatusCode)
+	}
+	t.Logf("res status %d", res.StatusCode)
+}
